d35efa8d186292cea37a9ae92d7a3287e62e14a1
Web Worker <webworker1@protonmail.com>
2019-11-17 10:46:35 -0500

    revised disclaimer and about

--- src/articles/about.js
+++ src/articles/about.js
@@ -6,7 +6,7 @@ class About extends React.Component {
     }
 
     render() {
-        return (<div>
+        return (<div className="article-wrapper">
                     Hi, I'm Zack.  I was a professional investor for seven years working as a high yield bond analyst, long/short equity hedge fund analyst, and then spent some time back again as a hybrid of the two roles.
                     I'm now a software engineer, and am finally free to write about the buyside.
                 </div>)
