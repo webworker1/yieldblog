c841bd96d2b48b55c9fd7ae31136149929d4cc48
webworker1 <webworker1@protonmail.com>
2019-12-26 10:03:43 -0500

    opens to new window

--- src/tags/img.js
+++ src/tags/img.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
-const imgLink = ({src, href, widthOverride}) => {
-    let width = "540px"
-    if (window.mobilecheck()) {
-        width = "480px"
+class ImageLink extends React.Component {
+    constructor(props) {
+        super(props);
+        this.width = "540px"
+        if (window.mobilecheck()) {
+            this.width = "480px"
+        }
     }
+    
+    newWindow() {
+        window.open(this.props.src || this.props.href)
+    }
+
+    render() {
+    let { widthOverride, src, href } = this.props;
     return (
-        <div>
-            <img style={{width: widthOverride || width }} src={src || href} /> <br/>
+        <div onClick={() => this.newWindow()}>
+            <img style={{ "width": this.width || widthOverride }} src={src || href} /> <br />
         </div>)
+    }
 }
 
-export default imgLink;
+
+export default ImageLink;
