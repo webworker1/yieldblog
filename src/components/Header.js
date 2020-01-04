7362fce93d520baebaf5d543ae7cbe6e570205b7
Zack <zackyang@gmail.com>
2020-01-04 00:04:53 -0500

    saving

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,21 @@ import { AwesomeButton } from 'react-awesome-button'
 class Header extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            dropdown: false
+        }
+        this.toggleDropdown = this.toggleDropdown.bind(this);
+        this.renderDropdown =  this.renderDropdown.bind(this);
+    }
+
+    toggleDropdown() {
+        console.log('toggling')
+        console.log(this.state)
+        this.setState({dropdown: !this.state.dropdown});
+    }
+
+    renderDropdown() {
+        return (<div> Hello Nurse </div>);
     }
 
     render() {
@@ -17,7 +32,8 @@ class Header extends React.Component {
                     <a href="/">Main</a>
                     <a href="/subscribe">Subscribe</a>
                     <a href="https://www.patreon.com/yieldblog">Patreon</a>
-                    <AwesomeButton onClick={() => this.renderOptions()} type="primary"><i class="fas fa-bars"></i></AwesomeButton>
+                    <AwesomeButton onPress={() => this.toggleDropdown()} type="primary"><i class="fas fa-bars"></i></AwesomeButton>
+                    {this.state.dropdown ? this.renderDropdown() : null}
                 </div>)
     }
 }
