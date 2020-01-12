fb4a56d95d58fdf5bff98b9986335caee97f3bec
Zack <zackyang@gmail.com>
2020-01-12 01:22:50 -0500

    added email

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,6 +36,7 @@ class Header extends React.Component {
         return (<div className="header-wrapper">
                     <a href="/">Main</a>
                     <a href="https://www.patreon.com/yieldblog">Patreon</a>
+                    <a href="https://mailchi.mp/2e4499d2d670/theyieldblog">Subscribe</a>
                     <div className='vertical-menu'>
                         <AwesomeButton onPress={() => this.toggleDropdown()} type="primary"><i class="fas fa-bars"></i></AwesomeButton>
                         {this.state.dropdown ? this.renderDropdown() : null}
