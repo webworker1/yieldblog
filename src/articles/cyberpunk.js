457edabd9a536e4cdb4f5d64a72cab5c1b4fbad3
Zack <zackyang@gmail.com>
2020-01-25 13:21:38 -0500

    skeletor

--- src/articles/cyberpunk.js
+++ src/articles/cyberpunk.js
@@ -12,7 +12,7 @@ class Cyberpunk extends React.Component {
         return (<div className="article-wrapper">
                     <br/>Developing a new AAA video game franchise reminds me a lot of building a cathedral to a new religion: most people will visit beautiful cathedrals, no matter their faith, but retaining worshippers and devotees requires creating a deep and abiding belief in the system at hand.
                     GTA V famously cost $265m to develop, while recently-constructed LA cathedral <Link href="https://en.wikipedia.org/wiki/Cathedral_of_Our_Lady_of_the_Angels_(Los_Angeles)">Cathedral of Our Lady of the Angels</Link> cost a mere $250m.
-                    <br/><br/>CD Projekt Red has a history of building cathedrals to existing religions, with their <Link href="https://en.wikipedia.org/wiki/The_Witcher_3:_Wild_Hunt">Witcher</Link> game series based on Andrzej Sapkowski's books of the same name.
+                    <br/><br/>CD Projekt Red has a history of building cathedrals to existing faiths, with their <Link href="https://en.wikipedia.org/wiki/The_Witcher_3:_Wild_Hunt">Witcher</Link> game series based on Andrzej Sapkowski's books of the same name.
                     CD Projekt Red's newest series, <Link href="https://amzn.to/36sBU7j">Cyberpunk 2077</Link> continues their tradition of finding barely-known source material stepped in a pre-existing genre and building
                     a much more famous game from its foundation -- in this case drawing on a little-known tabletop RPG, <Link href="https://talsorianstore.com/collections/cyberpunk/products/cyberpunk-2020">Cyberpunk</Link>, and more generally drawing on a universe first envisioned by the extraordinary William Gibson
                     in <Link href="https://www.amazon.com/gp/product/0441569595/ref=as_li_qf_asin_il_tl?ie=UTF8&tag=theyieldblog-20&creative=9325&linkCode=as2&creativeASIN=0441569595&linkId=3cc5191340fa10f1637a4a7934700f60">Neuromancer</Link> and the Sprawl trilogy.
@@ -25,7 +25,7 @@ class Cyberpunk extends React.Component {
                     Remember, Mojang, creator of the  <Link href="https://en.wikipedia.org/wiki/List_of_best-selling_video_games">best-selling video game of all time</Link>, Minecraft, was sold for only $2.5bn USD.
                     I was curious to see what assumptions it takes to earn enough cash to justify this valuation, so I started plugging in some numbers to a wild-ass-guess of a model:
                     <Image href="https://i.imgur.com/ZDalteC.gif" />
-                    If we assume that Cyberpunk goes on to one of the best-selling games of all time, selling 70m units, and then sells a sequel that performs similarly, we will see total cash earned of around 18bn PLN.  Does that mean that CD Projekt Red is overvalued? Not necessarily:  if CD Projekt Red can create another piece of compelling IP by 2026, they can easily maintain the "stub" valuation -- roughly speaking, 26bn PLN less the 18bn PLN that will be sitting on the balance sheet.
+                    If we assume that Cyberpunk goes on to become one of the best-selling games of all time, selling 70m units, and then sells a sequel that performs similarly, we will see total cash earned of around 18bn PLN.  Does that mean that CD Projekt Red is overvalued? Not necessarily:  if CD Projekt Red can create another piece of compelling IP by 2026, they can easily maintain the "stub" valuation -- roughly speaking, 26bn PLN less the 18bn PLN that will be sitting on the balance sheet.
 
                     However, for CD Projekt Red's stock to be a slam-dunk buy, Cyberpunk 2077 will need to become the next GTA V, <u>twice</u>:
                     <Image href="https://i.imgur.com/MxV4KIF.gif" />
@@ -37,8 +37,8 @@ class Cyberpunk extends React.Component {
                     <br/>1. CD Projekt Red needs to start working on its next IP <u>today</u>, so that it's ready by the time Cyberpunk 2077 IP is exhausted.  Given how little it spends in development (merely $100m USD a year), and given how much money Cyberpunk is expected to earn, this should be no problem at all, especially given their $200m USD of cash on the balance sheet.
                     <br/>2. The market is already pricing in Cyberpunk 2077 and its sequel to be one of the most successful games of all time.  
                     <br/>3. #1 and #2 are possible and credible:  we have the install base, we have the technology.  
-
-
+                    Hopefully wealthy and overworked CEO Marcin Iwinski will not become a diamond-encrusted Polish Skeletor by then:
+                    <Image href="https://i.imgur.com/2qdVxDq.png" />
                 </div>)
     }
 }
